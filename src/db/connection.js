8392054621a1b747a7cd881136c6314ac7541128
Guidy ange isaac <packer02andro69@gmail.com>
2025-03-12 00:01:14 +0000

    mise ajour des modifications

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,9 @@
 import {Sequelize} from "sequelize"
 import {configs} from "../utils/config.js";
+import cls from "cls-hooked";
 
+const nameSpace = cls.createNamespace("transaction");
+Sequelize.useCLS(nameSpace);
 export const sequelize = new Sequelize(configs.DB_NAME, configs.NAME, configs.DB_PASSWORD, {
     host: configs.DB_HOST,
     port: configs.DB_PORT,
@@ -9,11 +12,12 @@ export const sequelize = new Sequelize(configs.DB_NAME, configs.NAME, configs.DB
 
 export const connection = async function() {
     try {
-        //await sequelize.authenticate()
-        await sequelize.sync({alter: true})
+        await sequelize.authenticate()
+        //await sequelize.sync()
         console.log("Connected to database")
     }catch (error) {
         console.error(error)
     }
 }
 
+const transaction = await sequelize.transaction();
