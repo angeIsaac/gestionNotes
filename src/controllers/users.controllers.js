7aef33ee19e9cb78fd6b1233544e44469a074eb0
Guidy ange isaac <packer02andro69@gmail.com>
2025-03-17 12:59:00 +0000

    ajout de la methode deletUser

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -2,26 +2,64 @@ import { sequelize } from "../db/connection.js";
 import { Users} from "../db/models/users.js";
 
 export const getAllUsers = async (req, res) => {
-    const allUsers = await Users.findAll();
-    return res.status(200).json(allUsers);
+    try{
+        const allUsers = await Users.findAll();
+        return res.status(200).json(allUsers);
+    }catch(err){
+        return res.status(500).json(err);
+    }
 }
 
 export const getUsersById = async (req, res) => {
-    const id = req.params.id;
-    if (!id) {
-        return res.status(404).json({"message": "No such user with id " + id});
+    try{
+        const id = req.params.id;
+        if (!id) {
+            return res.status(404).json({"message": "No such user with id " + id});
+        }
+        const allUsers = await Users.findByPk(id);
+        return res.status(200).json(allUsers);
+    }catch (error){
+        console.log(error)
     }
-    const allUsers = await Users.findByPk(id);
-    return res.status(200).json(allUsers);
 }
 
 export const createUser = async (req, res) => {
-    const users = await Users.create(req.body);
-    return res.status(200).json(users);
+    try{
+        const users = await Users.create(req.body);
+        return res.status(200).json(users);
+    }catch (error){
+        res.status(500).json({"message": "Error creating user with id " + error});
+    }
 }
 
 export const updateUser = async (req, res) => {
-    const id = req.params.id;
-   const userUpdate = await Users.update(id, req.body);
-   return res.status(200).json(userUpdate);
+    try{
+        const id = req.params.id;
+        const userUpdate = await Users.update(req.body, {
+            where: {
+                id: id,
+            },
+            returning: true,
+        });
+        return res.status(200).json(userUpdate);
+    }catch (error){
+        res.status(500).json({"message": "une erreur est survenue lors de mise ajour de l'utilisateur" + error});
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    try{
+        const id = req.params.id;
+        if (!id) {
+            return res.status(404).json({"message": "No such user with id " + id});
+        }
+        const deletUser = await Users.destroy({
+            where: {
+                id: id
+            }
+        })
+        return res.status(200).json({deletUser, message: "supression reussir"});
+    }catch (error){
+        res.status(500).json({"message": "Error deleting user with id " + error});
+    }
 }
