e61048d424c57f189d590a60819b548104804e4e
Guidy ange isaac <packer02andro69@gmail.com>
2025-03-23 11:30:26 +0000

    creation des fonctions CRUD pour l'enseignant

---
+++ src/controllers/enseignant.controllers.js
@@ -0,0 +1,33 @@
+import { Enseignant } from "../db/models/enseignant.js";
+import {Classe} from "../db/models/classe.js";
+
+export const getAllEnseignants = async (req, res) => {
+    try{
+        const allEnseignat = await Enseignant.findAll({include: Classe});
+        res.status(200).json(allEnseignat);
+    }catch(err){
+        return res.status(400).json({"erreur": err})
+    }
+}
+
+export const getEnseignantById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        if(!id){
+            res.status(404).json({"error": "Not Found"})
+        }
+        const enseignant = await Enseignant.findByPk(id);
+        res.status(200).json(enseignant);
+    }catch (err){
+        return res.status(500).json({"error": err})
+    }
+}
+
+export const createEnseignant = async (req, res) => {
+    try {
+        const enseignant = await Enseignant.create(req.body);
+        res.status(200).json(enseignant);
+    }catch (err){
+        return res.status(500).json({"error": err})
+    }
+}
