af36d8c7d161cde59fa8e02b6a6b47b755ecf1fb
Guidy ange isaac <packer02andro69@gmail.com>
2025-03-24 15:33:22 +0000

    ce commit va changer le type de la clé primaire en UUID

--- src/db/models/filliere.js
+++ src/db/models/filliere.js
@@ -6,9 +6,9 @@ export class Fillieres extends Model {}
 
 Fillieres.init({
     id: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.UUID,
         primaryKey: true,
-        autoIncrement: true,
+        defaultValue: DataTypes.UUIDV4,
         unique: true
     },
     nom: {
